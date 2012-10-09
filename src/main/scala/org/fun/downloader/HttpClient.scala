0ba38e8c2eacc7d74b5c55d7b343003b3921e32a
Kazi Abdullah Saikat <saikat@viatravel.no>
2012-10-09 15:11:02 +0200

    Add slf4j and some libraries

---
+++ src/main/scala/org/fun/downloader/HttpClient.scala
@@ -0,0 +1,9 @@
+package org.fun.downloader
+
+import java.net.URL
+
+class HttpClient {
+	def getContent(url: URL) : String = {
+	  null;
+	}
+}
---
+++ src/main/scala/org/fun/downloader/ScalaDownloader.scala
@@ -0,0 +1,27 @@
+package org.fun.downloader
+
+import java.net.URL
+import scala.collection.immutable
+
+import org.slf4j._
+
+object ScalaDownloader {
+  
+	def logger : Logger = LoggerFactory.getLogger(getClass())
+  
+	def main(argv: Array[String]) = {
+	  if (argv.length == 0) {
+	    logger.error("usage: program root-url")
+	    exit(-1)
+	  }
+	  
+	}
+}
+
+class ScalaDownloader(pRootURL: URL) {
+  def rootURL = pRootURL
+  
+  def getProbableVideoURLs : List[URL] = {
+    return Nil;
+  }
+}
